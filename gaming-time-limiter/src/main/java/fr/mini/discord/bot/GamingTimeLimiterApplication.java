48bdcbdc9a5b7b78213703a6da9626549da21e22
Minipasglop <dertest58@yahoo.fr>
2024-07-06 16:27:43 +0200

    chore: add spring boot & Discord4J dependencies

---
+++ gaming-time-limiter/src/main/java/fr/mini/discord/bot/GamingTimeLimiterApplication.java
@@ -0,0 +1,13 @@
+package fr.mini.discord.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingTimeLimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamingTimeLimiterApplication.class, args);
+    }
+
+}
---
+++ gaming-time-limiter/src/test/java/fr/mini/discord/bot/GamingTimeLimiterApplicationTest.java
@@ -0,0 +1,13 @@
+package fr.mini.discord.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingTimeLimiterApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
